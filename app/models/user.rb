57307568d3fe0ad0a4e0acb906e1add78b37b8a3
연지 조 <mitaetoo@yeonjiui-MacBookAir.local>
2025-07-02 19:44:28 +0900

    lint修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :reviews, dependent: :destroy #Userが削除されたら、関連するReviewも削除される
+  has_many :reviews, dependent: :destroy # Userが削除されたら、関連するReviewも削除される
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
 
-  def self.nationalities_i18n #enumのキーをi18nで翻訳したものを返す
+  def self.nationalities_i18n # enumのキーをi18nで翻訳したものを返す
     nationalities.keys.index_with { |key| I18n.t("enums.user.nationality.#{key}") }
   end
 
