58547d312cc9d5123e629e7739aeb6148983227f
연지 조 <mitaetoo@yeonjiui-MacBookAir.local>
2025-07-04 01:47:20 +0900

    fix:レビュー投稿可能にする

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,17 +8,19 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    @review_form = Review.new
+    @review_form = ReviewForm.new
   end
 
   def create
-    @review_form = current_user.reviews.build(review_forms_params)
+    @review_form = ReviewForm.new(review_form_params)
     @review_form.user_id = current_user.id
     if @review_form.save
       redirect_to reviews_path
     else
       render :new
     end
+    # puts @review_form.errors.full_messages
+    # puts @review_form.params.inspect
   end
 
   def edit
@@ -32,7 +34,7 @@ class ReviewsController < ApplicationController
 
   private
 
-  def review_forms_params
-    params.require(:review_form).permit(:bod, :rating, :user_id, :product_id, :name, :price, :is_official)
+  def review_form_params
+    params.require(:review_form).permit(:body, :rating, :user_id, :product_id, :name, :price, :is_official)
   end
 end
