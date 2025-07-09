2b7ff972927ff2db044d3464e53dcba0d12dffc7
연지 조 <mitaetoo@yeonjiui-MacBookAir.local>
2025-07-09 18:01:41 +0900

    add:中間テーブルの作成(taste)

---
+++ app/models/review_taste.rb
@@ -0,0 +1,2 @@
+class ReviewTaste < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_05_023816) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_09_083251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_05_023816) do
     t.index ["review_id"], name: "index_review_categories_on_review_id"
   end
 
+  create_table "review_tastes", force: :cascade do |t|
+    t.bigint "review_id", null: false
+    t.bigint "taste_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["review_id"], name: "index_review_tastes_on_review_id"
+    t.index ["taste_id"], name: "index_review_tastes_on_taste_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.text "body"
     t.float "rating"
@@ -55,6 +64,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_05_023816) do
     t.bigint "product_id", null: false
     t.index ["product_id"], name: "index_reviews_on_product_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "tastes", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -75,6 +90,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_05_023816) do
   add_foreign_key "products", "conveniencestores"
   add_foreign_key "review_categories", "categories"
   add_foreign_key "review_categories", "reviews"
+  add_foreign_key "review_tastes", "reviews"
+  add_foreign_key "review_tastes", "tastes"
   add_foreign_key "reviews", "products"
   add_foreign_key "reviews", "users"
 end
