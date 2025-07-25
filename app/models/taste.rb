4da8863b4ac65963b5c9d7a8b5b030d89e2ea1da
연지 조 <mitaetoo@yeonjiui-MacBookAir.local>
2025-07-25 10:27:09 +0900

    add:tasteの翻訳完了

--- app/models/taste.rb
+++ app/models/taste.rb
@@ -1,4 +1,33 @@
 class Taste < ApplicationRecord
   has_many :review_tastes
   has_many :reviews, through: :review_tastes
+
+  def localized_name
+    translation_key = convert_name_to_key(name)
+    I18n.t("tastes.#{translation_key}", default: name)
+  end
+
+  private
+
+  def convert_name_to_key(name)
+    name_to_key_mapping = {
+      "甘い" => "sweet",
+    "少し甘い" => "little_sweet",
+    "とても甘い" => "too_sweet",
+    "辛い" => "spicy",
+    "少し辛い" => "little_spicy",
+    "とても辛い" => "too_spicy",
+    "しょっぱい" => "salty",
+    "少ししょっぱい" => "little_salty",
+    "とてもしょっぱい" => "too_salty",
+    "すっぱい" => "sour",
+    "少しすっぱい" => "little_sour",
+    "とてもすっぱい" => "too_sour",
+    "苦い" => "bitter",
+    "少し苦い" => "little_bitter",
+    "とても苦い" => "too_bitter",
+    "その他" => "other"
+  } 
+    name_to_key_mapping[name] || name.downcase.gsub(/[^a-z0-9]/, '_')
+  end
 end
