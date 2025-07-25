5e67795e7caa1b55fbe651dab14fa4f9197efc1d
연지 조 <mitaetoo@yeonjiui-MacBookAir.local>
2025-07-25 15:10:13 +0900

    fix:validates_errora

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,20 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy # Userが削除されたら、関連するReviewも削除される
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   enum nationality: { kr: 0, jp: 1 }
   enum language: { ko: 0, ja: 1 }
 
   validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6, message: I18n.t('errors.messages.minimum') }
+  validate :unique_email
+  private
+
+  def unique_email
+    if User.where.not(id: id).exists?(email: email)
+      errors.add(:email, I18n.t('errors.messages.taken'))
+    end
+  end
 
   def self.nationalities_for_select
     nationalities.keys.map do |key|
